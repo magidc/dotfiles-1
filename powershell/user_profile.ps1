bfd7bcabe215a3b46e38c0c124cfd9523d22bb18
AxewBoTX <84615670+AxewBoTX@users.noreply.github.com>
2023-01-25 21:29:51 +0530

    Add files via upload

---
+++ powershell/user_profile.ps1
@@ -0,0 +1,7 @@
+#Powershell Prompt`
+oh-my-posh init pwsh --config 'C:\Users\Axew\AppData\Local\Programs\oh-my-posh\themes\axew.omp.json' | Invoke-Expression
+
+# Alias
+Set-Alias open explorer
+Set-Alias touch New-Item
+Set-Alias rm Remove-Item
