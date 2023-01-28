511138292a9e75ff75f4ab772d2f6ac46adf3900
AxewBoTX <84615670+AxewBoTX@users.noreply.github.com>
2023-01-28 16:43:07 +0530

    Update user_profile.ps1

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -1,6 +1,9 @@
 #Powershell Prompt`
 oh-my-posh init pwsh --config 'C:\Users\Axew\AppData\Local\Programs\oh-my-posh\themes\axew.omp.json' | Invoke-Expression
 
+#Changing Neovim Config Directory
+$env:XDG_CONFIG_HOME = "$HOME/.config"
+
 # Alias
 Set-Alias open explorer
 Set-Alias touch New-Item
